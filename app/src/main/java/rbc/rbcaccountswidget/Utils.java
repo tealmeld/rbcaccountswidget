8009678a921d7e5be920ffbf57262e742a722323
tealmeld <tealmeld@users.noreply.github.com>
2015-11-08 02:52:57 -0500

    Retweaking network code

--- app/src/main/java/rbc/rbcaccountswidget/Utils.java
+++ app/src/main/java/rbc/rbcaccountswidget/Utils.java
@@ -9,6 +9,14 @@ public class Utils {
         String s = value.toString();
         int len = s.length();
         s = s.substring(0, len - 2) + "." + s.substring(len - 2, len);
-        return s;
+        len = len - 2;
+        int j = 0;
+        for (int i = len; i > 3; i--) {
+            if ((len - i) % 3 == 0) {
+                s = s.substring(0, i-3) + " " + s.substring(i-3, s.length());
+                j = j + 1;
+            }
+        }
+        return "$ " + s;
     }
 }
